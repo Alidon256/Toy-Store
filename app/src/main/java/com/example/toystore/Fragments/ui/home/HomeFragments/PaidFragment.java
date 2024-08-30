226c2be68fbb95f91c3e912cf2e0f45873129b94
ANHONY <mugumyaali@outlook.com>
2024-08-30 21:00:26 +0300

    Initial commit

--- app/src/main/java/com/example/toystore/Fragments/ui/home/HomeFragments/PaidFragment.java
+++ app/src/main/java/com/example/toystore/Fragments/ui/home/HomeFragments/PaidFragment.java
@@ -39,6 +39,7 @@ public class PaidFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
       //initializing the recycler view
         initializeRecyclerView();
+        // initializing the itemTouch helper
         ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT) {
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
